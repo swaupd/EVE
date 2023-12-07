bf3405d420da1760addb6dd1442175f41fd454db
swaupd <boricpowderacid@gmail.com>
2023-12-07 23:27:28 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
-const Stack = createNativeStackNavigator();
+
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { useFonts } from "expo-font";
+import axios from 'axios';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { View, Text, Pressable, TouchableOpacity } from "react-native";
+import { LogContextProvider } from "./contexts/LogContext"; // Import the LogContextProvider component
+
 import IPhone81 from "./screens/IPhone81";
 import Home2 from "./screens/Home2";
 import Homepage1 from "./screens/Homepage1";
@@ -10,12 +15,9 @@ import SignIn from "./screens/SignIn";
 import Frame from "./components/Frame";
 import Homepage from "./screens/Homepage";
 
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { View, Text, Pressable, TouchableOpacity } from "react-native";
+const Stack = createNativeStackNavigator();
 
 const App = () => {
-  const [hideSplashScreen, setHideSplashScreen] = React.useState(true);
-
   const [fontsLoaded, error] = useFonts({
     "Lalezar-Regular": require("./assets/fonts/Lalezar-Regular.ttf"),
     "Dosis-Regular": require("./assets/fonts/Dosis-Regular.ttf"),
@@ -23,49 +25,49 @@ const App = () => {
     "Dosis-Bold": require("./assets/fonts/Dosis-Bold.ttf"),
   });
 
-  if (!fontsLoaded && !error) {
-    return null;
+  if (!fontsLoaded) {
+    return null; // or return a loading screen
   }
 
   return (
-    <>
-      <NavigationContainer>
-        {hideSplashScreen ? (
-          <Stack.Navigator screenOptions={{ headerShown: false }}>
-            <Stack.Screen
-              name="IPhone81"
-              component={IPhone81}
-              options={{ headerShown: false }}
-            />
-            <Stack.Screen
-              name="Home2"
-              component={Home2}
-              options={{ headerShown: false }}
-            />
-            <Stack.Screen
-              name="Homepage1"
-              component={Homepage1}
-              options={{ headerShown: false }}
-            />
-            <Stack.Screen
-              name="Confirmation"
-              component={Confirmation}
-              options={{ headerShown: false }}
-            />
-            <Stack.Screen
-              name="SignIn"
-              component={SignIn}
-              options={{ headerShown: false }}
-            />
-            <Stack.Screen
-              name="Homepage"
-              component={Homepage}
-              options={{ headerShown: false }}
-            />
-          </Stack.Navigator>
-        ) : null}
-      </NavigationContainer>
-    </>
+    <NavigationContainer>
+    <LogContextProvider>
+      <Stack.Navigator initialRouteName='SignIn' screenOptions={{ headerShown: false }}>
+        <Stack.Screen
+          name="SignIn"
+          component={SignIn}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="IPhone81"
+          component={IPhone81}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Home2"
+          component={Home2}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Homepage1"
+          component={Homepage1}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Confirmation"
+          component={Confirmation}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Homepage"
+          component={Homepage}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+      </LogContextProvider>
+    </NavigationContainer>
   );
 };
+
 export default App;
+
